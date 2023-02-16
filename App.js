76ab981e834064a30a928fd5afc10b225a243184
eyamoah <enoch.kwabena@cellulant.io>
2023-02-16 15:01:12 +0000

    First Commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import ForwardCounter from "./components/ForwardCounter";
+import BackwardCounter from "./components/BackwardCounter";
+
+function App() {
+  return (
+    <div className="App">
+      <ForwardCounter />
+      <BackwardCounter />
+    </div>
+  );
+}
+
+export default App;
